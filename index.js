a15757fc9990bbb576a904d8f2e9457fed7f3fd5
P3ntest <juliusvanvoorden@outlook.com>
2020-02-17 18:31:01 +0100

    fixed code completely

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var express = require('express')
-  , http = require('http');
-var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 var interface_module = require('./interface.js');
-
 var interface = new interface_module(144);
 
 app.use(express.static('public'));
@@ -16,7 +13,7 @@ var y = 5;
 interface.setPixel(5, 5, 255, 0, 0);
 interface.updateScreen();
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/test.html');
 });
 
@@ -31,7 +28,6 @@ io.on('connection', function(socket){
   })
 });
 
-//server.listen(3000);
-app.listen(3000, function () {
-  console.log('PixelOS Web listening on port 3000!');
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
