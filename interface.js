c6430900b77243efb4d706ab8e08ef896a3b2e0e
Robin <lopfi95@gmail.com>
2020-02-17 19:51:58 +0100

    add clear screen

--- interface.js
+++ interface.js
@@ -1,11 +1,13 @@
 var ws281x = require('rpi-ws281x-v2');
+var PNG = require('png-js');
+
 class Interface {
   updateScreen() {
       ws281x.render(this.pixels);
   }
 
   translatePixelCoordinates(x, y) {
-      return y * Math.sqrt(this.pixel_amount) + x;
+      return y * this.side + x;
   }
 
   setPixel(x, y, r, g, b) {
@@ -18,7 +20,17 @@ class Interface {
     ws281x.configure({leds:pixels, gpio:18, strip:'rgb'});
     this.updateScreen();
     this.pixels = new Uint32Array(pixels);
+    this.side = Math.sqrt(this.pixel_amount);
   }
+
+  clearScreen() {
+    for (let x, x <= this.side, x++) {
+      for (let y, y <= this.side, y++) {
+        setPixel(x,y,0,0,0);
+      }
+    }
+  }
+
 }
 
 module.exports = Interface
