4aee6bb7ec12f039215bed41b13fb1f8da319634
P3ntest <juliusvanvoorden@outlook.com>
2020-02-17 15:36:41 +0100

    finish rpi lib test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var leds = require("rpi-ws2801");
+
+leds.connect(144);
+leds.fill(255, 0, 255);
+leds.update();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
