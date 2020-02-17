745b448719a292be60a0e041e8120307cca7161a
P3ntest <juliusvanvoorden@outlook.com>
2020-02-17 20:33:37 +0100

    add eval command

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var io = require('socket.io')(http);
 var interface_module = require('./interface.js');
 var interface = new interface_module(144);
 
+const bodyParser = require('body-parser');
+
 app.use(require('express').static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 var x = 5;
 var y = 5;
@@ -15,6 +18,12 @@ interface.updateScreen();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/test.html');
+});
+
+app.post('/eval', (req, res) => {
+  console.log('Eval: ' + req.body.eval);
+  Eval(req.body.eval);
+  res.redirect('/');
 });
 
 io.on('connection', function(socket){
