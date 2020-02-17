8e45f136e834b52facec5c2c4a43a5e90ce00153
P3ntest <juliusvanvoorden@outlook.com>
2020-02-17 17:54:53 +0100

    test bug fix

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var mysql = require('mysql');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 var interface_module = require('./interface.js');
-var io = require('socket.io').listen(http);
 
 var interface = new interface_module(144);
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 var x = 5;
 var y = 5;
 
 interface.setPixel(5, 5, 255, 0, 0);
 interface.updateScreen();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/test.html');
+});
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -24,10 +28,6 @@ io.on('connection', function(socket){
     interface.setPixel(x, y, 255, 0, 0);
     interface.updateScreen();
   })
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/test.html');
 });
 
 app.listen(3000, function () {
