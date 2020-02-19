a3a61552c6b72ae82bf75681ab83aedcc0d35570
P3ntest <juliusvanvoorden@outlook.com>
2020-02-19 16:04:59 +0100

    bug fix

--- modules/pacman/module.js
+++ modules/pacman/module.js
@@ -22,15 +22,15 @@ class PacManPlayer {
 class Map {
   constructor() {
     this.map = new Array();
-    for (int x = 0; x < 12; x++) {
+    for (let x = 0; x < 12; x++) {
       this.map.push(new Array());
-      for (int y = 0; y < 12; y++) {
+      for (let y = 0; y < 12; y++) {
         this.map[x].push(((Math.random() * 5) > 5) ? 1 : 0);
       }
     }
     this.food = new Array();
-    for (int x = 0; x < 12; x++) {
-      for (int y = 0; y < 12; y++) {
+    for (let x = 0; x < 12; x++) {
+      for (let y = 0; y < 12; y++) {
         if (this.map[x][y] == 0) {
           food.push({'x': x, 'y': y});
         }
