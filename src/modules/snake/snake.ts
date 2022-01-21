c32969fc76ee8b50b277e6ee27a115a05485fc05
P3ntest <juliusvanvoorden@outlook.com>
2022-01-21 19:27:14 +0100

    updates to movement system of snake

--- src/modules/snake/snake.ts
+++ src/modules/snake/snake.ts
@@ -1,3 +1,4 @@
+import { runInThisContext } from "vm";
 import gameManager, { Game, GameInstance, Ticker } from "../../gameManager";
 import PixelOS from "../../pixelOs";
 import { Color } from "../../screenInterface";
@@ -19,7 +20,7 @@ export const SnakeGame: Game = {
 
 class SnakeInstance implements GameInstance {
   position: [number, number] = [2, 2];
-  direction: Direction;
+  direction: Direction = Direction.CENTER;
 
   onStart(): void {
     PixelOS.getInterface().fill(new Color(0, 255, 0)).update();
@@ -31,14 +32,27 @@ class SnakeInstance implements GameInstance {
       }
     );
 
-    new Ticker(1, this, () => {
-      // const [dX, dY] = getDirectionDelta(this.direction);
-      // this.position[0] += dX;
-      // this.position[1] += dY;
-      // PixelOS.getInterface()
-      //   .clear()
-      //   .setPixel(this.position[0], this.position[1], new Color(255, 0, 0))
-      //   .update();
+    new Ticker(10, this, () => {
+      // console.log(
+      //   this.direction,
+      //   directionDelta,
+      //   getDirectionDelta(this.direction)
+      // );
+      console.log(typeof Direction.CENTER, typeof this.direction);
+      console.log(Direction.CENTER, this.direction);
+      const delta = getDirectionDelta(this.direction);
+      // console.log(delta);
+      this.position[0] += delta[0];
+      this.position[1] += delta[1];
+
+      this.position[0] =
+        this.position[0] % PixelOS.getInstance().screenInterface.width;
+      this.position[1] =
+        this.position[1] % PixelOS.getInstance().screenInterface.height;
+      PixelOS.getInterface()
+        .clear()
+        .setPixel(this.position[0], this.position[1], new Color(255, 0, 0))
+        .update();
     });
   }
   gameManager: gameManager;
