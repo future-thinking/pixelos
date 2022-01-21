5a176c7ec171aefe4c7a4fe848018cf627f5c434
P3ntest <p3ntest@gmx.de>
2022-01-21 08:49:04 +0100

    add backend url hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,26 @@ import './App.css'
 
 
 function App() {
+  const [response, setResponse] = useState('No response')
+
   useEffect(() => {
-    const socket = socketIOClient("http://localhost:8000");
+    const socket = socketIOClient(useBackend());
+    socket.on('msg', (data: string) => {
+      setResponse(data)
+    });
   }, [])
 
   return (
-   <>Hallo</>
+   <>{response}</>
   )
+}
+
+function useBackend(): string {
+  if (import.meta.env.MODE === 'development') {
+    return 'http://localhost:8000'
+  } else {
+    return window.location.origin
+  }
 }
 
 export default App
